062e1f1674343bea41a28d78e928d719e2e6a05f
Pulkit Gulati <pulkit.gemini@gmail.com>
2018-05-29 00:22:24 +0530

    code for placeholder

---
+++ window-shopper/window-shopper/CurrencyTxtField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Pulkit Gulati on 26/05/18.
+//  Copyright © 2018 Pulkit Gulati LLC. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [NSForegroundColorAttributeName: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+    }
+
+}
